9e15cb7143d42251b99955eca2ecfa20975af415
ColinJNoden <colin.noden@sky.com>
2021-12-03 10:24:13 +0000

    Completed Code

--- Script.js
+++ Script.js
@@ -1,3 +1,12 @@
 const randomMessageGenerator = () => {
+    const secOne = ['Your Mum','Your Dad','Your breath','Your nose','Your face'];
+    const secTwo = ['likes','eats','smells like','gargles'];
+    const secThree = ['sweaty balls','tramps feet', 'old mans crotch','Donald Trump\'s wig glue'];
+
+    return `${secOne[Math.floor(Math.random()*secOne.length)]} ${secTwo[Math.floor(Math.random()*secTwo.length)]} ${secThree[Math.floor(Math.random()*secThree.length)]}`
+
+
 
 }
+
+console.log(randomMessageGenerator());
